0721a10f00a60c88923f8d4ec822335c06628766
JunTaeINC <juntea3523@naver.com>
2023-08-16 22:16:48 +0900

    feat: view request Info

---
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -0,0 +1,42 @@
+package hello.servlet.basic.request;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
+public class RequestHeaderServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws
+		ServletException, IOException {
+		printStartLine(request);
+		printHeaders(request);
+	}
+
+	private void printStartLine(HttpServletRequest request) {
+		System.out.println("--- REQUEST-LINE - start ---");
+		System.out.println("request.getMethod() = " + request.getMethod());
+		System.out.println("request.getProtocol() = " + request.getProtocol());
+		System.out.println("request.getScheme() = " + request.getScheme());
+		System.out.println("request.getRequestURL() = " + request.getRequestURL());
+		System.out.println("request.getRequestURI() = " + request.getRequestURI());
+		System.out.println("request.getQueryString() = " + request.getQueryString());
+		System.out.println("request.isSecure() = " + request.isSecure());
+		System.out.println("--- REQUEST-LINE - end ---");
+		System.out.println();
+	}
+
+	private void printHeaders(HttpServletRequest request) {
+		System.out.println("--- Header - Start ---");
+
+		request.getHeaderNames().asIterator()
+			.forEachRemaining(headerName -> System.out.println(headerName + " : " + headerName));
+
+		System.out.println("--- Header - End ---");
+	}
+}
