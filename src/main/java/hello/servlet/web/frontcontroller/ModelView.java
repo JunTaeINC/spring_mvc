a345bdc4b470a4d9fb044ede796680df9c0f69fc
JunTaeINC <juntea3523@naver.com>
2023-08-23 22:35:51 +0900

    refactor: apply v4 to v5 with adapter

--- src/main/java/hello/servlet/web/frontcontroller/ModelView.java
+++ src/main/java/hello/servlet/web/frontcontroller/ModelView.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @Setter
 public class ModelView {
 	private final String viewName;
-	private final Map<String, Object> model = new HashMap<>();
+	private Map<String, Object> model = new HashMap<>();
 
 	public ModelView(String viewName) {
 		this.viewName = viewName;
